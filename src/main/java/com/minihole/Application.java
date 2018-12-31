415e0662999d07beff5eda6b33a57c0730d7ac27
Json <1353933051@qq.com>
2018-12-31 13:29:05 +0800

    first commit

---
+++ src/main/java/com/minihole/Application.java
@@ -0,0 +1,18 @@
+package com.minihole;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by Mr.w on 2018/7/6.
+ * 注册中心启动入口
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
